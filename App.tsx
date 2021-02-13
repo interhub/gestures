fc008f104e13afe1414933a5b044fbfb4798dd35
stepanturchenko <stepanstepan4@gmail.com>
2021-02-13 19:21:59 +0200

    add refactor video box state

--- App.tsx
+++ App.tsx
@@ -1,30 +1,12 @@
-import { Video } from 'expo-av';
 import * as React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import VideoBox from './src/VideoBox';
 
 export default function App() {
-  const video = React.useRef<Video>(null);
-  const [status, setStatus] = React.useState(false);
-  return (
 
+  return (
     <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status ? 'Pause' : 'Play'}
-          onPress={() => {
-            setStatus(!status)
-            status ? video.current?.pauseAsync() : video.current?.playAsync()
-          }}
-        />
-      </View>
+      <VideoBox />
     </View>
   );
 }
@@ -35,11 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  video: {
-    width: 300, height: 200
-  },
-  buttons: {
-    height: 200
   }
 });
