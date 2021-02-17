77fd1109289c0707867e89ef4b472d61bf23d5f2
stepanturchenko <stepanstepan4@gmail.com>
2021-02-17 17:43:06 +0200

    add save full scroll native

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import VideoBox from './src/VideoBox';
 const TextItem = () => {
   const { height, width } = useWindowDimensions()
   const SIZE = width
-  return <View style={{ borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', width: SIZE, height: SIZE, backgroundColor: '#4649ad', }} >
+  return <View style={{ borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', width: SIZE, height: SIZE * 4, backgroundColor: '#4649ad', }} >
     <Text style={{ color: '#fff', }} >
       Hello world!
     </Text>
--- src/GesturBox.tsx
+++ src/GesturBox.tsx
@@ -1,5 +1,5 @@
 
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Animated, PanResponder, useWindowDimensions, } from 'react-native';
 import clamp from 'clamp'
 
@@ -14,11 +14,15 @@ const GesturBox = ({ children }: GesturBoxProps) => {
 
     const pan = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current
 
+    const val = Animated.diffClamp(pan, -100, 100)
+
     const panGesture = useRef(PanResponder.create({
         onMoveShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
             pan.setOffset({
+                // @ts-ignore 
                 x: pan.x._value,
+                // @ts-ignore
                 y: pan.y._value
             });
             pan.setValue({ x: 0, y: 0 })
@@ -31,30 +35,29 @@ const GesturBox = ({ children }: GesturBoxProps) => {
             { useNativeDriver }
         ),
         onPanResponderRelease: (e, { vx, vy }) => {
-            // console.log('REALESE', e)
             pan.flattenOffset();
-            let velocity: any
-            if (vx >= 0) {
-                velocity = clamp(vx, 0.1, 2);
-            } else if (vx < 0) {
-                velocity = clamp(vx, 0.1, 0.5) * -1;
-            }
-
-            Animated.decay(pan, { velocity: { x: velocity, y: vy }, deceleration: 0.997, useNativeDriver }).start(() => {
-                // pan.setOffset({ x: 0, y: 0 })
+            Animated.decay(pan, { velocity: { x: vx, y: vy }, deceleration: 0.997, useNativeDriver }).start(() => {
                 console.log('END ANIM PAN')
             })
         },
     })).current
 
+    // useEffect(() => {
+    //     pan.addListener((e) => {
+    //         console.log(e, 'e')
+    //     })
+    // }, [])
+
     const { height, width } = useWindowDimensions()
 
-    const translateY = pan.y.interpolate({ inputRange: [0, height], outputRange: [0, height * 2], extrapolate: 'clamp' })
+    const contentSize = 1500
+
+    const translateY = pan.y.interpolate({ inputRange: [-contentSize, 0], outputRange: [-3 * contentSize, 0], extrapolate: 'clamp' })
     const rotateX = pan.y.interpolate({ inputRange: [0, height], outputRange: ['0deg', '360deg'], extrapolate: 'clamp' })
 
     return (
         <Animated.View {...panGesture.panHandlers} style={{ flex: 1, backgroundColor: 'lightblue' }} >
-            <Animated.View style={{ transform: [{ translateY }, { rotateX }], }} >
+            <Animated.View style={{ transform: [{ translateY }], }} >
                 {children}
             </Animated.View>
         </Animated.View>
