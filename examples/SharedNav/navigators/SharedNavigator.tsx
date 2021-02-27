01b492aa927cbce3d725e5c80c280a44daa141c2
stepanturchenko <stepanstepan4@gmail.com>
2021-02-27 10:49:18 +0200

    add debug install full react and share element libs

--- examples/SharedNav/navigators/SharedNavigator.tsx
+++ examples/SharedNav/navigators/SharedNavigator.tsx
@@ -1,10 +1,11 @@
 import {createSharedElementStackNavigator} from 'react-navigation-shared-element'
 import {SCREEN_NAME_SHARED} from '../constants/SCREEN_NAME_SHARED'
 import Start from '../screen/Start'
+import React from 'react'
 import ItemDetail from '../screen/ItemDetail'
 
 const Stack = createSharedElementStackNavigator()
- 
+
 const App = () => {
     return <Stack.Navigator headerMode={'none'}>
         <Stack.Screen name={SCREEN_NAME_SHARED.START_LIST} component={Start}/>
--- examples/SharedNav/screen/ItemDetail.tsx
+++ examples/SharedNav/screen/ItemDetail.tsx
@@ -12,6 +12,8 @@ const ItemDetail = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 })
 
