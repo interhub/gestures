13b1994b1293cda18fe742c687b720225e9b1b94
stepanturchenko <stepanstepan4@gmail.com>
2021-02-27 10:19:47 +0200

    add sin animate to scroll

--- examples/ReanimateGesture/components/GesturBox.tsx
+++ examples/ReanimateGesture/components/GesturBox.tsx
@@ -1,6 +1,6 @@
 import clamp from 'clamp'
-import React from 'react'
-import {useWindowDimensions, View} from 'react-native'
+import React, {useEffect, useRef} from 'react'
+import {Button, ScrollView, Text, useWindowDimensions, View} from 'react-native'
 import {
     GestureHandlerGestureEvent,
     GestureHandlerStateChangeEvent,
@@ -22,25 +22,53 @@ const AnimatedScrollView = Animated.ScrollView
 
 const GesturBox = ({children}: GesturBoxProps) => {
 
-    const {height, width} = useWindowDimensions()
-
     const scrollY = useSharedValue(0)
 
     const scrollHandler = useAnimatedScrollHandler((event) => {
         scrollY.value = event.contentOffset.y
+        // console.log(scrollY.value)
     })
 
-    const boxStyle = useAnimatedStyle(() => ({
-        transform: [{translateY: scrollY.value}]
-    }))
+    // const boxStyle = useAnimatedStyle(() => ({
+    //     transform: [{translateY: scrollY.value * 2}],
+    //     position: 'absolute',
+    //     top: 0,
+    // }))
+    const {width, height} = useWindowDimensions()
 
+    const pageSize = height * 2
+
+    const ref = useRef<ScrollView>(null)
+
+    const scrollTo = (size: number, startPos = 1, speed = 5) => {
+        if (startPos >= size) return
+        const speedDiff = Math.sin(2 * Math.PI / startPos) * 10000
+        console.log(speedDiff, 'diff')
+        ref.current?.scrollTo({x: 0, y: speedDiff, animated: false})
+        startPos += speed
+        requestAnimationFrame(() => scrollTo(size, startPos))
+    }
+
+    const onScrollPress = () => {
+        const SCROLL_SIZE = 600
+        scrollTo(SCROLL_SIZE)
+    }
+
+
+    const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at atque blanditiis eum fugit libero molestias numquam praesentium quae quisquam, reiciendis sapiente voluptates voluptatum? Aliquam consequuntur expedita fugiat itaque vero!'
     return (
         <View style={{flex: 1, backgroundColor: 'lightblue'}}>
-            <AnimatedScrollView onScroll={scrollHandler}>
-                <Animated.View style={boxStyle}>
-                    {children}
-                </Animated.View>
-            </AnimatedScrollView>
+            <ScrollView
+                ref={ref}
+                style={{backgroundColor: 'red'}}
+                scrollEventThrottle={5}>
+                <View style={{height: pageSize, backgroundColor: '#4649ad', paddingTop: 200}}>
+                    <Button color={'red'} title={'scroll'} onPress={onScrollPress}/>
+                    <Text>
+                        {text}
+                    </Text>
+                </View>
+            </ScrollView>
         </View>
     )
 }
