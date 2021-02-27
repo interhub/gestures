5bdec73c42496e052b5e6d6d90e1c902765e4fec
stepanturchenko <stepanstepan4@gmail.com>
2021-02-27 09:27:28 +0200

    change app name and add shared start element page and change reanimated on scroll view

--- App.tsx
+++ App.tsx
@@ -1,22 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import * as React from 'react';
-import 'react-native-gesture-handler';
-import NativeGesture from './examples/NativeGesture';
-import ReanimateGesture from './examples/ReanimateGesture';
-import { SCREEN_NAME } from './src/SCREEN_NAME';
-import Menu from './src/Menu';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import * as React from 'react'
+import 'react-native-gesture-handler'
+import NativeGesture from './examples/NativeGesture'
+import ReanimateGesture from './examples/ReanimateGesture'
+import SharedNav from './examples/SharedNav'
+import {SCREEN_NAME} from './src/SCREEN_NAME'
+import Menu from './src/Menu'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const App = () => {
-  return <NavigationContainer>
-    <Stack.Navigator headerMode={'none'} >
-      <Stack.Screen name={SCREEN_NAME.MENU} component={Menu} />
-      <Stack.Screen name={SCREEN_NAME.NATIVE_GESTURE} component={NativeGesture} />
-      <Stack.Screen name={SCREEN_NAME.REANIMATE_GESTURE} component={ReanimateGesture} />
-    </Stack.Navigator>
-  </NavigationContainer>
+    return <NavigationContainer>
+        <Stack.Navigator headerMode={'none'}>
+            <Stack.Screen name={SCREEN_NAME.MENU} component={Menu}/>
+            <Stack.Screen name={SCREEN_NAME.NATIVE_GESTURE} component={NativeGesture}/>
+            <Stack.Screen name={SCREEN_NAME.REANIMATE_GESTURE} component={ReanimateGesture}/>
+            <Stack.Screen name={SCREEN_NAME.SHARED_NAV} component={SharedNav}/>
+        </Stack.Navigator>
+    </NavigationContainer>
 }
 
 export default App
--- examples/ReanimateGesture/components/GesturBox.tsx
+++ examples/ReanimateGesture/components/GesturBox.tsx
@@ -1,60 +1,48 @@
-
-import clamp from 'clamp';
-import React from 'react';
-import { useWindowDimensions, View } from 'react-native';
-import { GestureHandlerGestureEvent, GestureHandlerStateChangeEvent, PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
-import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withDecay } from 'react-native-reanimated';
+import clamp from 'clamp'
+import React from 'react'
+import {useWindowDimensions, View} from 'react-native'
+import {
+    GestureHandlerGestureEvent,
+    GestureHandlerStateChangeEvent,
+    PanGestureHandler,
+    PanGestureHandlerGestureEvent
+} from 'react-native-gesture-handler'
+import Animated, {
+    useAnimatedGestureHandler, useAnimatedScrollHandler,
+    useAnimatedStyle,
+    useSharedValue,
+    withDecay
+} from 'react-native-reanimated'
 
 interface GesturBoxProps {
     children: React.ReactNode
 }
 
-const GesturBox = ({ children }: GesturBoxProps) => {
+const AnimatedScrollView = Animated.ScrollView
 
-    const { height, width } = useWindowDimensions()
+const GesturBox = ({children}: GesturBoxProps) => {
 
-    const contentSize = 1500
+    const {height, width} = useWindowDimensions()
 
-    const translate = useSharedValue(0)
+    const scrollY = useSharedValue(0)
 
-    const clamp: [number, number] = [-height, height]
-    const isInClamp = (val: number) => {
-        "worklet";
-        return val > clamp[0] && val < clamp[1]
-    }
-
-    const onGestureEvent = useAnimatedGestureHandler<PanGestureHandlerGestureEvent, { ctx: number }>({
-        onStart: ({ translationY }, env) => { console.log('START'); env.ctx = translate.value },
-        onActive: ({ translationY }, { ctx = 0 }) => {
-            console.log('ACTIVE');
-            console.log(isInClamp(translationY + ctx), 'isInClamp(translationY + ctx)')
-            if (!isInClamp(translationY + ctx)) return;
-            translate.value = translationY + ctx;
-        },
-        onEnd: ({ velocityY }) => { translate.value = withDecay({ velocity: velocityY, clamp }) },
-        onCancel: () => {
-            console.log('CANCELEd')
-        },
-        onFail: () => { console.log('FAIL TOUCH') }
+    const scrollHandler = useAnimatedScrollHandler((event) => {
+        scrollY.value = event.contentOffset.y
     })
 
     const boxStyle = useAnimatedStyle(() => ({
-        transform: [{ translateY: translate.value * 3 }]
+        transform: [{translateY: scrollY.value}]
     }))
 
     return (
-        <View style={{ flex: 1, backgroundColor: 'lightblue' }}>
-            <PanGestureHandler onGestureEvent={onGestureEvent}  >
-                <Animated.View
-                    onTouchStart={() => {
-                        translate.value = withDecay({ velocity: 0 })
-                    }}
-                    style={[boxStyle]} >
+        <View style={{flex: 1, backgroundColor: 'lightblue'}}>
+            <AnimatedScrollView onScroll={scrollHandler}>
+                <Animated.View style={boxStyle}>
                     {children}
                 </Animated.View>
-            </PanGestureHandler>
+            </AnimatedScrollView>
         </View>
-    );
+    )
 }
 
 console.log(clamp(0, 0.1, 2))
